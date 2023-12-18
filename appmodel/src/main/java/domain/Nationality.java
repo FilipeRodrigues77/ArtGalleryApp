05e82458edcb916b398cfc1c1a9fde32d0fad1ad
nuefurth <nuefurth@gmail.com>
2023-12-18 17:00:40 +0000

    added default images for artwork and artist - doc nationality class

--- appmodel/src/main/java/domain/Nationality.java
+++ appmodel/src/main/java/domain/Nationality.java
@@ -1,5 +1,15 @@
 package domain;
 
+
+/**
+ * The Nationality class represents a person's nationality and provides methods to get and set
+ * the nationality value. Instances of this class can be used to store and retrieve information
+ * about an individual's nationality.
+ *
+ * @author Nuely Furtado
+ * @author Filipe Alves
+ * @version v1.0
+ */
 public class Nationality {
 
     private String nationality;
